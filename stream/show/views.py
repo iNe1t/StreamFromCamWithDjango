5f7e9dfa7e2e0a5abc37ba239ce11927bb11271f
iNe1t <fantiktop212@gmail.com>
2021-06-10 16:17:51 +0300

    deleted:    firstmp4.mp4 	deleted:    firstvideo.avi 	deleted:    stream/show/haarcascade_frontalface_default.xml 	modified:   stream/stream/settings.py 	modified:   stream/templates/stream.html

--- stream/show/views.py
+++ stream/show/views.py
@@ -1,52 +1,22 @@
 from django.views.decorators import gzip
 from django.http import StreamingHttpResponse
 from picamera import PiCamera
+from imutils.video import VideoStream, FPS
+
 import cv2
 import threading
-#раскоментить и протестировать
-# import io
-# import picamera
-# import cv2
-# import numpy
+import io
+import numpy as np
+import imutils
+import time
 
-# #Create a memory stream so photos doesn't need to be saved in a file
-# stream = io.BytesIO()
 
-# #Get the picture (low resolution, so it should be quite fast)
-# #Here you can also specify other parameters (e.g.:rotate the image)
-# with picamera.PiCamera() as camera:
-#     camera.resolution = (320, 240)
-#     camera.capture(stream, format='jpeg')
-
-# #Convert the picture into a numpy array
-# buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
-
-# #Now creates an OpenCV image
-# image = cv2.imdecode(buff, 1)
-
-# #Load a cascade file for detecting faces
-# face_cascade = cv2.CascadeClassifier('/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml')
-
-# #Convert to grayscale
-# gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-
-# #Look for faces in the image using the loaded cascade file
-# faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-
-# print "Found "+str(len(faces))+" face(s)"
-
-# #Draw a rectangle around every found face
-# for (x,y,w,h) in faces:
-#     cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
-
-# #Save the result image
-# cv2.imwrite('result.jpg',image)
 
 class VideoCamera(object):
     def __init__(self):
-        camera = PiCamera()
-        self.video = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-        (self.grabbed, self.frame) = self.video.read()
+        self.face_cascade = cv2.CascadeClassifier('/home/pi/Desktop/StreamFromCamWithDjango/stream/show/recog.xml')
+        self.video = cv2.VideoCapture(0) #VideoStream(usePiCamera = True).start()
+        self.grabbed, self.frame = self.video.read()
         threading.Thread(target=self.update, args=()).start()
 
     def __del__(self):
@@ -54,14 +24,26 @@ class VideoCamera(object):
 
     def get_frame(self):
         image = self.frame
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        #resize the image
+        width = int(image.shape[1] * 50/100)
+        height = int(image.shape[0] * 50/100)
+        dsize = (width, height)
+        image = cv2.resize(image, dsize)
+        
+        faces = self.face_cascade.detectMultiScale(image, 1.1, 5)
+        print("Found " + str(len(faces)) + " face(s)")
+        
+        for (x,y,w,h) in faces:
+                cv2.rectangle(image, (x,y), (x+w, y+h), (255,255,0), 2)
+        
         _, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
 
     def update(self):
         while True:
-            (self.grabbed, self.frame) = self.video.read()
-
-
+            self.grabbed, self.frame = self.video.read()
+            
 def gen(camera):
     while True:
         frame = camera.get_frame()
